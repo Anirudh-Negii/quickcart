cab80077496c138f23feca506f6ca6ea67db93b2
Anirudh <anirudh.formal88@gmail.com>
2026-09-25 16:12:45 +0530

    Add frontend routing

--- frontend/src/app/App.jsx
+++ frontend/src/app/App.jsx
@@ -1,11 +1,23 @@
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router";
+import Home from "../pages/Home";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+import Products from "../pages/Products";
+import ProductDetails from "../pages/ProductDetails";
 
-const App = () => {
+function App() {
   return (
-    <div>
-      <h1 className="text-4xl font-bold text-red-500">QuickCart</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/products/:id" element={<ProductDetails />} />
+      </Routes>
+    </BrowserRouter>
   );
-};
+}
 
 export default App;
